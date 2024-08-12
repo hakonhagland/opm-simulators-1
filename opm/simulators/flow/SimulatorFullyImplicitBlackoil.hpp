d257291a35312b78d0ce0dc7b0c14859f87248dd
Håkon Hægland <hakon.hagland@gmail.com>
2024-08-12 15:17:38 +0200

    Send slave start dates to master

--- opm/simulators/flow/SimulatorFullyImplicitBlackoil.hpp
+++ opm/simulators/flow/SimulatorFullyImplicitBlackoil.hpp
@@ -241,6 +241,7 @@ public:
                         this->schedule()
                     );
                 this->reservoirCouplingMaster_->spawnSlaveProcesses(argc, argv);
+                this->reservoirCouplingMaster_->receiveSimulationStartDateFromSlaves();
             }
         }
         simulator_.setEpisodeIndex(-1);
