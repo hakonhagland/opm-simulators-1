OPM/opm-simulators

Communicate group potentials
Send group potentials from the slaves to the master process, then
use the potentials in the master process to calculate guide rates.

CMakeLists_files.cmake
@@ -204,6 +204,7 @@ list (APPEND MAIN_SOURCE_FILES
   opm/simulators/wells/BlackoilWellModelRestart.cpp
   opm/simulators/wells/BlackoilWellModelWBP.cpp
   opm/simulators/wells/ConnFiltrateData.cpp
+  opm/simulators/wells/GuideRateHandler.cpp
   opm/simulators/wells/FractionCalculator.cpp
   opm/simulators/wells/GasLiftCommon.cpp
   opm/simulators/wells/GasLiftGroupInfo.cpp
@@ -1067,6 +1068,7 @@ list (APPEND PUBLIC_HEADER_FILES
   opm/simulators/wells/GlobalWellInfo.hpp
   opm/simulators/wells/GroupEconomicLimitsChecker.hpp
   opm/simulators/wells/GroupState.hpp
+  opm/simulators/wells/GuideRateHandler.hpp
   opm/simulators/wells/MSWellHelpers.hpp
   opm/simulators/wells/MultisegmentWell.hpp
   opm/simulators/wells/MultisegmentWell_impl.hpp
@@ -1269,7 +1271,7 @@ if(MPI_FOUND)
     opm/simulators/flow/ReservoirCouplingMaster.hpp
     opm/simulators/flow/ReservoirCouplingSlave.hpp
     opm/simulators/flow/ReservoirCouplingSpawnSlaves.hpp
-  )
+    )
   list (APPEND TEST_SOURCE_FILES
     tests/rescoup/test_chopstep.cpp
   )

hakonhagland/opm-simulators-1

CMakeLists_files.cmake
@@ -213,6 +213,7 @@ list (APPEND MAIN_SOURCE_FILES
   opm/simulators/wells/GlobalWellInfo.cpp
   opm/simulators/wells/GroupEconomicLimitsChecker.cpp
   opm/simulators/wells/GroupState.cpp
+  opm/simulators/wells/GroupTargetCalculator.cpp
   opm/simulators/wells/MSWellHelpers.cpp
   opm/simulators/wells/MultisegmentWellAssemble.cpp
   opm/simulators/wells/MultisegmentWellEquations.cpp
@@ -1077,6 +1078,7 @@ list (APPEND PUBLIC_HEADER_FILES
   opm/simulators/wells/GlobalWellInfo.hpp
   opm/simulators/wells/GroupEconomicLimitsChecker.hpp
   opm/simulators/wells/GroupState.hpp
+  opm/simulators/wells/GroupTargetCalculator.hpp
   opm/simulators/wells/GuideRateHandler.hpp
   opm/simulators/wells/MSWellHelpers.hpp
   opm/simulators/wells/MultisegmentWell.hpp
@@ -1274,6 +1276,8 @@ if(MPI_FOUND)
     opm/simulators/flow/ReservoirCouplingMaster.cpp
     opm/simulators/flow/ReservoirCouplingSlave.cpp
     opm/simulators/flow/ReservoirCouplingSpawnSlaves.cpp
+    opm/simulators/wells/RescoupReceiveGroupTargets.cpp
+    opm/simulators/wells/RescoupTargetCalculator.cpp
   )
   list (APPEND PUBLIC_HEADER_FILES
     opm/simulators/flow/ReservoirCoupling.hpp
@@ -1281,6 +1285,8 @@ if(MPI_FOUND)
     opm/simulators/flow/ReservoirCouplingMaster.hpp
     opm/simulators/flow/ReservoirCouplingSlave.hpp
     opm/simulators/flow/ReservoirCouplingSpawnSlaves.hpp
+    opm/simulators/wells/RescoupReceiveGroupTargets.hpp
+    opm/simulators/wells/RescoupTargetCalculator.hpp
     )
   list (APPEND TEST_SOURCE_FILES
     tests/rescoup/test_chopstep.cpp