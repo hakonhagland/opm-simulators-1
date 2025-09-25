1a335e3048e030349df00f2f8cc6b00530fec0bd
Håkon Hægland <hakon.hagland@gmail.com>
2025-09-25 08:13:05 +0200

    Add integration points for slave-to-master rate communication
    - Move master receiving of slave data to earlier location (before updateAndCommunicateGroupData)
    - Add receiveSlavesGroupRates() call for master
    - Add sendSlaveGroupRatesToMaster() call for slave after guide rate calculation
    - Ensures proper timing: slaves send after potentials are calculated

--- opm/simulators/wells/BlackoilWellModel_impl.hpp
+++ opm/simulators/wells/BlackoilWellModel_impl.hpp
@@ -375,6 +375,14 @@ namespace Opm {
             // create the well container
             createWellContainer(reportStepIdx);
 
+#ifdef RESERVOIR_COUPLING_ENABLED
+            // Receive all slave group data early to ensure it's available for any calculations
+            if (this->isReservoirCouplingMaster()) {
+                this->guide_rate_handler_.receiveMasterGroupPotentialsFromSlaves();
+                this->receiveSlavesGroupRates(this->groupState(), reportStepIdx);
+            }
+#endif
+
             // we need to update the group data after the well is created
             // to make sure we get the correct mapping.
             this->updateAndCommunicateGroupData(reportStepIdx,
@@ -454,11 +462,6 @@ namespace Opm {
             local_deferredLogger.warning("WELL_POTENTIAL_CALCULATION_FAILED", msg);
         }
         this->guide_rate_handler_.setLogger(&local_deferredLogger);
-#ifdef RESERVOIR_COUPLING_ENABLED
-        if (this->isReservoirCouplingMaster()) {
-            this->guide_rate_handler_.receiveMasterGroupPotentialsFromSlaves();
-        }
-#endif
         //update guide rates
         this->guide_rate_handler_.updateGuideRates(
             reportStepIdx, simulationTime, this->wellState(), this->groupState()
@@ -466,6 +469,7 @@ namespace Opm {
 #ifdef RESERVOIR_COUPLING_ENABLED
         if (this->isReservoirCouplingSlave()) {
             this->guide_rate_handler_.sendSlaveGroupPotentialsToMaster(this->groupState());
+            this->sendSlaveGroupRatesToMaster(this->groupState(), reportStepIdx);
         }
 #endif
         std::string exc_msg;
